7f8c13545754151036b6cf6f192ab351151bfd01
SystProf <97409223+SystProf@users.noreply.github.com>
2022-01-18 08:37:17 -0500

    arraysize changed
    year added in comment

--- src/StudentList/StudList.java
+++ src/StudentList/StudList.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author Megha Patel
+ * @author Megha Patel,2022
  */
 public class StudList {
 
@@ -17,7 +17,7 @@ public class StudList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Student[] s1=new Student[3];//declare student object array
+        Student[] s1=new Student[5];//declare student object array
         Scanner input=new Scanner(System.in);
         
         for(int i=0; i<s1.length;i++)
